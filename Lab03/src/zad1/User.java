58ec699fecbedd9d66649a6d616d4085b6d18dcf
chelicery <s15198@pjwstk.edu.pl>
2020-12-06 11:55:55 +0100

    Lab03 added

---
+++ Lab03/src/zad1/User.java
@@ -0,0 +1,50 @@
+package zad1;
+public class User {
+
+	private int userId;
+	private Account account;
+	private String name;
+	private String surname;
+	private String adress;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+}
